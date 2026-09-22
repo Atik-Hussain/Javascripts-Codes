a58c2baec5e792fcb3bc0b9c967c68fefb95bf28
Atik-Hussain <axatikking@gmail.com>
2026-09-23 02:20:51 +0530

    initial

---
+++ student_information.js
@@ -0,0 +1,28 @@
+let std_name="Atik Hussain";
+let age=19;
+var cousre="BCA";
+var cllge_name="SITM";
+let marks=80;
+let birth_year=2006;
+
+let city="Guwahati";
+let semester="1st Semester";
+let favourite_subject="Programming Language";
+
+age=21;
+marks=85;
+
+console.log("Student Name :",std_name);
+console.log("Age :",age);
+console.log("Course :",cousre);
+console.log("College Name :",cllge_name);
+console.log("Marks Obtained :",marks);
+console.log("Date of Birth :",birth_year);
+console.log("City :",city);
+console.log("Semester :",semester);
+console.log("Favourite subject :",favourite_subject);
+
+
+
+
+
